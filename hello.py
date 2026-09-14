adf30f07667e62f19162cacdfa488b49d7a86887
solist7 <aizensosuke694@gmail.com>
2026-09-14 12:41:26 +0000

    dfdfd

--- hello.py
+++ hello.py
@@ -1,5 +1,14 @@
-print('hello world')
-a = 10
-b = 24
-res = a + b 
-print('result',res)
+##print('hello world')
+##a = 10
+##b = 24
+##res = a + b 
+##print('result',res)
+a,b=int(input()),int(input())
+if a < 0 and b < 0 :
+    print(min(a,b))
+else:
+    print('>0')
+res = a+b
+if res %3==0 and res%5==0:
+    print('YES')
+    
